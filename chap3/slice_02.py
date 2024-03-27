31687e1d89e6eca67f1e7c0bfdcd9e07719fccbc
kanehiro <kanehiro@gmail.com>
2024-03-27 15:49:40 +0900

    Add files via upload

---
+++ chap3/slice_02.py
@@ -0,0 +1,18 @@
+str1 = 'Python'
+
+str_new = str1[2:5]
+print(str_new)
+
+str1 = 'プログラミング学習'
+
+str_new = str1[7:]
+print(str_new)
+
+str_new = str1[:7]
+print(str_new)
+
+str_new = str1[2:5]
+print(str_new)
+
+str_new = str1[::-1]
+print(str_new)
---
+++ chap3/tpl_01.py
@@ -0,0 +1,7 @@
+tpl_01 = (1,2,3,4.65,True,'こんにちは')
+
+movies = ('PERFECT DAYS', 'ヴァチカンのエクソシスト', 'ゴジラ-1.0')
+
+print(movies[1])
+
+movies[0] = '君たちはどう生きるか'  #Error
